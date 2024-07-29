3f83396277efd94ea4d81dc7139156909fc0bd96
DavidPizarro151 <paulpizarro151@gmail.com>
2024-07-28 23:14:57 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://davidpizarro151.github.io',
-  base: '/davidpizarro.github.io',
+  site: 'https://davidpizarro.github.io',
   integrations: [tailwind()]
 });
